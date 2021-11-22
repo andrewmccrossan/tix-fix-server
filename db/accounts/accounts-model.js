48a3a15881c6d7669e7523ec848e1c11bd466965
Andrew McCrossan <afmccrossan@gmail.com>
2021-11-22 02:10:39 -0500

    accounts ES6 schema import

--- db/accounts/accounts-model.js
+++ db/accounts/accounts-model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
-const schema = require('./accounts-schema');
+import { schema } from './accounts-schema';
+// const schema = require('./accounts-schema');
 const model = mongoose.model('AccountsModel', schema);
 module.exports = model;
