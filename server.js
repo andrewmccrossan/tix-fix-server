3d42c0fd39c1c43e89dcc916022c7bd57b277f86
Andrew McCrossan <afmccrossan@gmail.com>
2021-11-22 01:53:21 -0500

    accounts correct

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
-// const mongoose = require('mongoose');
-// const mongodb_uri = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+const mongodb_uri = process.env.MONGODB_URI;
 // await mongoose.connect(mongodb_uri);
+mongoose.connect(mongodb_uri, { useUnifiedTopology: true, useNewUrlParser: true });
+mongoose.connection.once("open", function() {
+    console.log("MongoDB database connection established successfully");
+});
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-// require('./services/accounts-service')(app);
+require('./services/accounts-service')(app);
 
 app.listen(process.env.PORT || 4000);
