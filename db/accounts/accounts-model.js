ef3edcbb350c9010a6c4b848640064bf6af1d9c5
Andrew McCrossan <afmccrossan@gmail.com>
2021-11-22 02:13:04 -0500

    accounts reverse ES6 import

--- db/accounts/accounts-model.js
+++ db/accounts/accounts-model.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-import { schema } from './accounts-schema';
-// const schema = require('./accounts-schema');
+const schema = require('./accounts-schema');
 const model = mongoose.model('AccountsModel', schema);
 module.exports = model;
