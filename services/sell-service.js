62020fcd080e493b07c0a70164040e994e1f15fa
jspunna <jspunna214@gmail.com>
2021-12-09 13:32:25 -0500

    adding sellerinfo, small updates

--- services/sell-service.js
+++ services/sell-service.js
@@ -4,18 +4,26 @@ module.exports = (app) => {
 
     const addToSellerEventsSelling = (req, res) => {
         const newTickets = req.body;
-        const seller = req.session['profile']._id.toString();
-        sellersDao.updateSellerEventsSelling(seller, newTickets)
+        const sellerID = req.session['profile']._id.toString();
+        sellersDao.updateSellerEventsSelling(sellerID, newTickets)
             .then((status) => res.json(status))
     };
 
     const addToSellerWatchList = (req, res) => {
         const eventID = req.body.eventID;
-        const seller = req.session['profile']._id.toString();
-        sellersDao.updateSellerEventsWatchlist(seller, eventID)
+        const sellerID = req.session['profile']._id.toString();
+        sellersDao.updateSellerEventsWatchlist(sellerID, eventID)
             .then((status) => res.json(status))
+    };
+
+    const sellerInfo = (req, res) => {
+        const sellerID = req.session['profile']._id.toString();
+        console.log(sellerID);
+        sellersDao.findSellerById(sellerID)
+            .then(info => {res.json(info); console.log(info)});
     };
 
     app.post('/sell/tickets', addToSellerEventsSelling);
     app.post('/sell/watchlist', addToSellerWatchList);
+    app.get('/sell/seller', sellerInfo);
 }
